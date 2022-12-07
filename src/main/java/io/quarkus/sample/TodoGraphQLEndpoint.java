de93558821f04af3e01915b5b40c054fddeb4d95
omri.ratson <omri.ratson@nice.com>
2022-12-07 15:01:56 +0200

    test

--- src/main/java/io/quarkus/sample/TodoGraphQLEndpoint.java
+++ src/main/java/io/quarkus/sample/TodoGraphQLEndpoint.java
@@ -9,22 +9,19 @@ import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
 
-import static io.quarkus.hibernate.orm.panache.PanacheEntityBase.findById;
-import static io.quarkus.hibernate.orm.panache.PanacheEntityBase.listAll;
-
 @GraphQLApi
 public class TodoGraphQLEndpoint {
 
     @Query
     @Description("Get all the todos")
     public List<Todo> getAllTodos() {
-        return listAll(Sort.by("order"));
+        return Todo.listAll(Sort.by("order"));
     }
 
     @Query
     @Description("Get a specific todo by id")
     public Todo getTodo(Long id) {
-        return findById(id);
+        return Todo.findById(id);
     }
 
     @Mutation
@@ -37,7 +34,7 @@ public class TodoGraphQLEndpoint {
     @Mutation
     @Description("Update an exiting todo")
     public Todo update(@Valid Todo todo, Long id) {
-        Todo entity = findById(id);
+        Todo entity = Todo.findById(id);
         entity.id = id;
         entity.completed = todo.completed;
         entity.order = todo.order;
@@ -57,7 +54,7 @@ public class TodoGraphQLEndpoint {
     @Mutation
     @Description("Delete a specific todo")
     public Todo deleteTodo(Long id) {
-        Todo entity = findById(id);
+        Todo entity = Todo.findById(id);
         if (entity == null) {
             return null;
         }
